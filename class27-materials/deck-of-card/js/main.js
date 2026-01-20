b537b4b6ac768197ae679bdc01f65f26c06746a3
chiomavera <you@example.com>
2026-01-20 12:54:55 +0100

    Space reptition, deleted the solution deck-of-card  (class 27) and worked through it again

--- class27-materials/deck-of-card/js/main.js
+++ class27-materials/deck-of-card/js/main.js
@@ -1,48 +1,48 @@
 // Pull data from https://deckofcardsapi.com/
 // try to see if can always use only a specific deck. maybe storing in local storage.
-
 let deckId = "";
 
-fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-  .then((res) => res.json()) // parse response as JSON
+fetch ("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1") 
+  .then((res) => res.json()) // parse response as json
   .then((data) => {
-    console.log(data);
-    deckId = data.deck_id;
-  })
-  .catch((err) => {
-    console.log(`error ${err}`);
-  });
+    console.log(data)
+    deckId = data.deck_id
+  }).catch( (err) => console.log(`Error: ${err}`))
 
-document.querySelector("button").addEventListener("click", drawTwo);
+  document.querySelector('button').addEventListener("click", drawAnotherCard)
 
-function drawTwo() {
-  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
+function drawAnotherCard () {
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
 
   fetch(url)
-    .then((res) => res.json()) // parse response as JSON
-    .then((data) => {
-      console.log(data);
-      document.querySelector("#player1").src = data.cards[0].image;
-      document.querySelector("#player2").src = data.cards[1].image;
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
 
-      let player1Val = convertToNum(data.cards[0].value);
-      let player2Val = convertToNum(data.cards[1].value);
+    document.querySelector("#player1").src = data.cards[0].image 
+      document.querySelector("#player1Suit").innerText = data.cards[0].suit
 
-      const result = document.querySelector("#result")
-      if (player1Val > player2Val) {
-        result.innerText = "Player 1 wins";
-      } else if (player1Val < player2Val) {
-        result.innerText = "player 2 wins";
-      } else {
-        result.innerText = "Time for War!";
-      }
-    })
-    .catch((err) => {
-      console.log(`error ${err}`);
-    });
+     document.querySelector("#player2").src = data.cards[1].image 
+      document.querySelector("#player2Suit").innerText = data.cards[1].suit
+
+     const player1Val = convertToNum(data.cards[0].value)
+     const player2Val = convertToNum(data.cards[1].value)
+
+     document.querySelector("#player1Val").innerText = `Value: ${player1Val}`
+       document.querySelector("#player2Val").innerText = `Value: ${player2Val}`
+    const result = document.querySelector("#result")
+
+     if (player1Val > player2Val) {
+      result.innerText = "Player 1 Wins"
+     }else if (player1Val < player2Val) {
+      result.innerText = "Player 2 Wins"
+     }else{
+      result.innerText = "Time for War"
+     }
+  })
 }
 
-function convertToNum(val) {
+function convertToNum (val) {
   if (val === "ACE") {
     return 14;
   } else if (val === "KING") {
